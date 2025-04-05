70af7aa1e98dc7332d187742f7c77176f2e8c344
PatrickHua <patrickhua.ty@gmail.com>
2025-04-04 22:40:55 -0700

    client update with openai:

--- core/async_chat_clients.py
+++ core/async_chat_clients.py
@@ -3,15 +3,9 @@ import logging
 import os
 from typing import List, Dict, Any, Optional
 
-from openai import AsyncOpenAI, RateLimitError as OpenAIRateLimitError
-import openai
-from anthropic import AsyncAnthropic, RateLimitError as AnthropicRateLimitError, InternalServerError
+from openai import AsyncOpenAI, RateLimitError
 
 from core.data_classes.llm_type import LLMType, MODEL_CONFIGS
-
-# Companies using specific clients
-COMPANIES_USING_OPENAI_CLIENT = ['OPENAI', 'VLLM', 'GOOGLE', 'XAI']
-COMPANIES_USING_ANTHROPIC_CLIENT = ['ANTHROPIC']
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -26,12 +20,15 @@ class AsyncChatClients():
         self.llm_clients = {
             
             'OPENAI': AsyncOpenAI(api_key=os.getenv('OPENAI_API_KEY')),
-            'ANTHROPIC': AsyncAnthropic(api_key=os.getenv('ANTHROPIC_API_KEY')),
+            'ANTHROPIC': AsyncOpenAI(api_key=os.getenv('ANTHROPIC_API_KEY'),
+                                    base_url="https://api.anthropic.com/v1"),
             'GOOGLE': AsyncOpenAI(api_key=os.getenv('GOOGLE_API_KEY'),
                                   base_url="https://generativelanguage.googleapis.com/v1beta/"),
             'XAI': AsyncOpenAI(api_key=os.getenv('XAI_API_KEY'),
                                base_url="https://api.x.ai/v1"),
             'VLLM': AsyncOpenAI(api_key='EMPTY', base_url="http://localhost:8000/v1"),
+            'DEEPSEEK': AsyncOpenAI(api_key=os.getenv('DEEPSEEK_API_KEY'),
+                                    base_url="https://api.deepseek.com/v1"),
         }
         self.all_responses = []
         self.total_inference_cost = 0
@@ -43,18 +40,12 @@ class AsyncChatClients():
         Calculates the cost of a response.
         """
         llm_config = MODEL_CONFIGS[llm_type]
-        company = llm_config.company
         input_cost = llm_config.input_cost / 1_000_000  # cost per 1M tokens
         output_cost = llm_config.output_cost / 1_000_000
         
-        if company in COMPANIES_USING_OPENAI_CLIENT:
-            input_tokens = response.usage.prompt_tokens
-            output_tokens = response.usage.completion_tokens
-        elif company in COMPANIES_USING_ANTHROPIC_CLIENT:
-            input_tokens = response.usage.input_tokens
-            output_tokens = response.usage.output_tokens
-        else:
-            raise ValueError(f"Unknown company: {company}")
+        input_tokens = response.usage.prompt_tokens
+        output_tokens = response.usage.completion_tokens
+
         
         try:
             cost = (input_cost * input_tokens) + (output_cost * output_tokens)
@@ -65,83 +56,57 @@ class AsyncChatClients():
         self.total_output_tokens += output_tokens
         return cost
 
-    def get_prompt(
-        self,
-        llm_type: LLMType,
-        user_message: str,
-        system_message: Optional[str],
-    ) -> List[Dict[str, str]]:
-        """
-        Constructs the prompt for the model.
-        """
-        model = llm_type.value
-        company = MODEL_CONFIGS[llm_type].company
+    # def get_prompt(
+    #     self,
+    #     llm_type: LLMType,
+    #     user_message: str,
+    #     system_message: Optional[str],
+    # ) -> List[Dict[str, str]]:
+    #     """
+    #     Constructs the prompt for the model.
+    #     """
 
-        if company == 'ANTHROPIC':
-            messages = [
-                system_message if system_message else [],
-                {"role": "user", "content": user_message},
-            ]
-        elif system_message is None or model in ['mistralai/Mixtral-8x7B-Instruct-v0.1']:
-            messages = [{"role": "user", "content": user_message}]
-        else:
-            messages = [
-                {"role": "system", "content": system_message},
-                {"role": "user", "content": user_message},
-            ]
-        return messages
+    #     messages = [
+    #         {"role": "system", "content": system_message},
+    #         {"role": "user", "content": user_message},
+    #     ]
+    #     return messages
 
-    async def get_response(
-        self,
-        llm_type: LLMType,
-        messages: List[Dict[str, str]],
-        temperature: Optional[float] = None,
-        max_tokens: Optional[int] = None,
-        timeout: Optional[int] = None,
-        max_retries: int = 10,
-        num_completions: int = 1,
-    ) -> Any:
-        """
-        Attempts to get a valid response from the model using retries with exponential backoff.
-        """
-        company = MODEL_CONFIGS[llm_type].company
-        model = llm_type.value
+    # async def get_response(
+    #     self,
+    #     llm_type: LLMType,
+    #     messages: List[Dict[str, str]],
+    #     temperature: Optional[float] = None,
+    #     max_tokens: Optional[int] = None,
+    #     timeout: Optional[int] = None,
+    #     max_retries: int = 10,
+    # ) -> Any:
+    #     """
+    #     Attempts to get a valid response from the model using retries with exponential backoff.
+    #     """
+    #     company = MODEL_CONFIGS[llm_type].company
+    #     model = llm_type.value
 
-        wait_time = 60
-        for attempt in range(1, max_retries + 1):
-            try:
-                if company in COMPANIES_USING_OPENAI_CLIENT:
-                    return await self.llm_clients[company].chat.completions.create(
-                        model=model,
-                        messages=messages,
-                        temperature=temperature,
-                        max_completion_tokens=max_tokens,
-                        timeout=timeout,
-                        n=num_completions
-                    )
-                elif company in COMPANIES_USING_ANTHROPIC_CLIENT:
-                    system_message, remaining_messages = messages[0], messages[1:]
-                    return await self.llm_clients[company].messages.create(
-                        model=model,
-                        messages=remaining_messages,
-                        temperature=temperature,
-                        max_tokens=max_tokens,
-                        system=system_message,
-                        timeout=timeout,
-                        n=num_completions
-                    )
-                else:
-                    raise ValueError(f"Unknown company: {company}")
-            except (OpenAIRateLimitError, AnthropicRateLimitError) as e:
-                logger.warning(f"Rate limit exceeded for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
-            except InternalServerError as e:
-                logger.warning(f"Internal server error for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
-            except Exception as e:
-                logger.error(f"Unexpected error for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
-            await asyncio.sleep(wait_time)
-            wait_time *= 1
+    #     wait_time = 60
+    #     for attempt in range(1, max_retries + 1):
+    #         try:
+    #             return await self.llm_clients[company].chat.completions.create(
+    #                     model=model,
+    #                     messages=messages,
+    #                     temperature=temperature,
+    #                     max_completion_tokens=max_tokens,
+    #                     timeout=timeout,
+    #                     n=1
+    #                 )
 
-        raise Exception(f"Failed to get a valid response from {company} after {max_retries} attempts.")
+    #         except RateLimitError as e:
+    #             logger.warning(f"Rate limit exceeded for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
+    #         except Exception as e:
+    #             logger.error(f"Unexpected error for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
+    #         await asyncio.sleep(wait_time)
+    #         wait_time *= 1
+
+    #     raise Exception(f"Failed to get a valid response from {company} after {max_retries} attempts.")
 
     async def run(
         self,
@@ -153,48 +118,52 @@ class AsyncChatClients():
         timeout: Optional[int] = None,
         return_full_response: bool = False,
         num_completions: int = 1,
+        max_retries: int = 10,
     ) -> Dict[str, Any]:
         """
         Runs the agent and returns the response along with cost information.
         """
         company = MODEL_CONFIGS[llm_type].company
-
-        if company in COMPANIES_USING_ANTHROPIC_CLIENT:
-            assert temperature is not None, 'temperature must be provided for Anthropic models'
-            assert max_tokens is not None, 'max_tokens must be provided for Anthropic models'
-        elif company == 'OPENAI' and llm_type.value.startswith('o'):
-            temperature = 1
-            max_tokens = max_tokens * 10 if max_tokens is not None else None
-
-        model = llm_type.value
-        messages = self.get_prompt(llm_type=llm_type, user_message=user_message, system_message=system_message)
+        client_kwargs = MODEL_CONFIGS[llm_type].client_kwargs
         
-        response = await self.get_response(
-            llm_type=llm_type,
-            messages=messages,
+        full_kwargs = dict(
+            model=llm_type.value,
+            messages=[
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": user_message},
+            ],
             temperature=temperature,
-            max_tokens=max_tokens,
+            max_completion_tokens=max_tokens,
             timeout=timeout,
-            num_completions=num_completions
-        )
+        ) | client_kwargs if client_kwargs else {}
+        
+        responses = []
+        response_strs = []
+        cost = 0
+        for _ in range(num_completions):
+            wait_time = 60
+            response = None
+            for attempt in range(1, max_retries + 1):
+                try:
+                    response = await self.llm_clients[company].chat.completions.create(**full_kwargs)
+                    break  # Success, exit retry loop
+                except RateLimitError as e:
+                    logger.warning(f"Rate limit exceeded for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
+                except Exception as e:
+                    logger.error(f"Unexpected error for {company} on attempt {attempt}/{max_retries}: {e}. Retrying in {wait_time} seconds.")
+                await asyncio.sleep(wait_time)
+                wait_time *= 1
 
-        # Validate response
-        if hasattr(response, 'choices') and response.choices is None:
-            logger.error(f"Invalid response received for model {model}: {response}")
-            raise ValueError(f"Invalid response received for model {model}")
+            if response is None:
+                raise Exception(f"Failed to get a valid response from {company} after {max_retries} attempts.")
 
-        # Calculate cost and parse response string
-        cost = self.calc_cost(response=response, llm_type=llm_type)
-        if company in COMPANIES_USING_OPENAI_CLIENT:
-            response_str = [choice.message.content for choice in response.choices]
-        elif company in COMPANIES_USING_ANTHROPIC_CLIENT:
-            response_str = [content.text for content in response.content]
-        else:
-            raise ValueError(f"Unknown company: {company}")
+            responses.append(response)
+            cost += self.calc_cost(response=response, llm_type=llm_type)
+            response_strs.append(response.choices[0].message.content)
 
         full_response = {
             'response': response,
-            'response_str': response_str,
+            'response_str': response_strs,
             'cost': cost
         }
         self.total_inference_cost += cost
@@ -202,7 +171,7 @@ class AsyncChatClients():
         if return_full_response:
             return full_response
         else:
-            return response_str
+            return response_strs
 
     async def batch_prompt(
         self,
@@ -228,11 +197,18 @@ async def main():
     clients = AsyncChatClients()
     try:
         output = await clients.run(
-            llm_type=LLMType.GEMINI_2_0_FLASH_LITE_PREVIEW_02_05,
-            user_message='Hello, how are you? What is lsikjdfgolw;ajg;o',
+            # llm_type=LLMType.GEMINI_2_0_FLASH_LITE_PREVIEW_02_05,
+            # llm_type=LLMType.O3_MINI_2025_01_31,
+            # llm_type=LLMType.CLAUDE_3_5_SONNET_2024_10_22,
+            # llm_type=LLMType.O3_MINI_HIGH,
+            # llm_type=LLMType.CLAUDE_3_7_SONNET_2025_02_19,
+            # llm_type=LLMType.O1_HIGH,
+            llm_type=LLMType.DEEPSEEK_R1,
+            user_message='Hello, how are you?',
             system_message='You are a helpful assistant.',
             num_completions=2,
-            temperature=0.5
+            temperature=0,
+            # max_tokens=4096,
         )
         print("Response:", output)
         breakpoint()
--- core/data_classes/llm_type.py
+++ core/data_classes/llm_type.py
@@ -18,13 +18,20 @@ class LLMType(str, Enum):
     GPT_4O_2024_11_20 = "gpt-4o-2024-11-20"
     GPT_4O_2024_08_06 = "gpt-4o-2024-08-06"
     GPT_4O_2024_05_13 = "gpt-4o-2024-05-13"
-    O1_MINI = "o1-mini"
+    # O1_MINI = "o1-mini"
+    O1="o1-2024-12-17"
+    O1_HIGH="o1-2024-12-17"
     O1_PREVIEW_2024_09_12 = "o1-preview-2024-09-12"
-    O3_MINI_2025_01_31_HIGH = "o3-mini-2025-01-31-high"
-    O3_MINI = "o3-mini"
-    O1 = "o1"  # not working
+    # O3_MINI_2025_01_31_HIGH = "o3-mini-2025-01-31-high"
+    O3_MINI_HIGH = "o3-mini-2025-01-31"
+    O3_MINI_2025_01_31 = "o3-mini-2025-01-31"
+    O3_MINI = "o3-mini-2025-01-31"  # same with o3-mini-2025-01-31
+    # O1 = "o1"  # not working
+    O1_MINI = "o1-mini-2024-09-12"
+    # O1_MINI_HIGH = "o1-mini-2024-09-12"
     
     # Anthropic models https://docs.anthropic.com/en/docs/about-claude/models
+    CLAUDE_3_7_SONNET_2025_02_19 = "claude-3-7-sonnet-20250219"
     CLAUDE_3_5_SONNET_2024_10_22 = "claude-3-5-sonnet-20241022"
     CLAUDE_3_5_HAIKU_2024_10_22 = "claude-3-5-haiku-20241022"
     CLAUDE_3_OPUS_2024_02_29 = "claude-3-opus-20240229"
@@ -42,13 +49,16 @@ class LLMType(str, Enum):
     # XAI models
     GROK_2_1212 = "grok-2-1212"
 
+    # DeepSeek models
+    DEEPSEEK_R1 = "deepseek-reasoner"
+
 
 class LLMConfig(BaseModel):
     company: str
     input_cost: Optional[float]
     output_cost: Optional[float]
     knowledge_cutoff_date: Optional[str] = None
-
+    client_kwargs: Optional[dict] = None
     # def get_api_key(self) -> str:
     #     if self.company == 'OPENAI':
     #         return os.getenv('OPENAI_API_KEY')
@@ -99,19 +109,27 @@ MODEL_CONFIGS = {
     LLMType.GPT_4O_2024_11_20: LLMConfig(company='OPENAI', input_cost=2.5, output_cost=10),
     LLMType.GPT_4O_2024_08_06: LLMConfig(company='OPENAI', input_cost=2.5, output_cost=10),
     LLMType.GPT_4O_2024_05_13: LLMConfig(company='OPENAI', input_cost=5, output_cost=15),
-    LLMType.O1_MINI: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4),
-    LLMType.O3_MINI_2025_01_31_HIGH: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4),
-    LLMType.O3_MINI: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4),
-    LLMType.O1_PREVIEW_2024_09_12: LLMConfig(company='OPENAI', input_cost=15, output_cost=60),
-    LLMType.O1: LLMConfig(company='OPENAI', input_cost=15, output_cost=60),
+    LLMType.O1_MINI: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1}),
+    # LLMType.O3_MINI_2025_01_31_HIGH: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1, 'reasoning': {"effort": "high"}}),
+
+
+    LLMType.O3_MINI_HIGH: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1, 'reasoning': {"effort": "high"}}),
+    LLMType.O3_MINI_2025_01_31: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1}),
+    LLMType.O3_MINI: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1}),
+    LLMType.O1_PREVIEW_2024_09_12: LLMConfig(company='OPENAI', input_cost=15, output_cost=60, client_kwargs={'temperature': 1}),
+    LLMType.O1: LLMConfig(company='OPENAI', input_cost=15, output_cost=60, client_kwargs={'temperature': 1}),
+    LLMType.O1_HIGH: LLMConfig(company='OPENAI', input_cost=15, output_cost=60, client_kwargs={'temperature': 1, 'reasoning': {"effort": "high"}}),
+    LLMType.O1_MINI: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1}),
+    # LLMType.O1_MINI_HIGH: LLMConfig(company='OPENAI', input_cost=1.1, output_cost=4.4, client_kwargs={'temperature': 1, 'reasoning': {"effort": "high"}}),
     
     # Anthropic models https://www.anthropic.com/pricing#anthropic-api
     # https://docs.anthropic.com/en/docs/about-claude/models
-    LLMType.CLAUDE_3_5_SONNET_2024_10_22: LLMConfig(company='ANTHROPIC', input_cost=3, output_cost=15),
-    LLMType.CLAUDE_3_5_HAIKU_2024_10_22: LLMConfig(company='ANTHROPIC', input_cost=0.8, output_cost=4),
-    LLMType.CLAUDE_3_OPUS_2024_02_29: LLMConfig(company='ANTHROPIC', input_cost=15, output_cost=75),
-    LLMType.CLAUDE_3_SONNET_2024_02_29: LLMConfig(company='ANTHROPIC', input_cost=3, output_cost=15),
-    LLMType.CLAUDE_3_HAIKU_2024_03_07: LLMConfig(company='ANTHROPIC', input_cost=0.25, output_cost=1.25),
+    LLMType.CLAUDE_3_7_SONNET_2025_02_19: LLMConfig(company='ANTHROPIC', input_cost=3, output_cost=15, client_kwargs={'max_completion_tokens': 8192}),
+    LLMType.CLAUDE_3_5_SONNET_2024_10_22: LLMConfig(company='ANTHROPIC', input_cost=3, output_cost=15, client_kwargs={'max_completion_tokens': 8192}),
+    LLMType.CLAUDE_3_5_HAIKU_2024_10_22: LLMConfig(company='ANTHROPIC', input_cost=0.8, output_cost=4, client_kwargs={'max_completion_tokens': 8192}),
+    LLMType.CLAUDE_3_OPUS_2024_02_29: LLMConfig(company='ANTHROPIC', input_cost=15, output_cost=75, client_kwargs={'max_completion_tokens': 4096}),
+    LLMType.CLAUDE_3_SONNET_2024_02_29: LLMConfig(company='ANTHROPIC', input_cost=3, output_cost=15, client_kwargs={'max_completion_tokens': 4096}),
+    LLMType.CLAUDE_3_HAIKU_2024_03_07: LLMConfig(company='ANTHROPIC', input_cost=0.25, output_cost=1.25, client_kwargs={'max_completion_tokens': 4096}),
 
     # https://ai.google.dev/pricing#2_0flash
     LLMType.GEMINI_2_0_FLASH: LLMConfig(company='GOOGLE', input_cost=0.1, output_cost=0.4),
@@ -120,7 +138,10 @@ MODEL_CONFIGS = {
     LLMType.GEMINI_1_5_FLASH_8B: LLMConfig(company='GOOGLE', input_cost=0.0375, output_cost=0.15),
     LLMType.GEMINI_1_5_PRO: LLMConfig(company='GOOGLE', input_cost=1.25, output_cost=5.00),
     # LLMType.GEMINI_1_0_PRO: LLMConfig(company='GOOGLE', input_cost=0.50, output_cost=1.50),
-    
+
+    LLMType.DEEPSEEK_R1: LLMConfig(company='DEEPSEEK', input_cost=0.14, output_cost=2.19, client_kwargs={'stream': False}),
+
+
     # https://x.ai/pricing
     LLMType.GROK_2_1212: LLMConfig(company='XAI', input_cost=2, output_cost=10, knowledge_cutoff_date="2024-07-17"),
 }
