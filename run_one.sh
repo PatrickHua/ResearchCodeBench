adc9f4a8286aee57fbe2ab6d83c58d7626d3592a
PatrickHua <patrickhua.ty@gmail.com>
2025-04-17 23:00:00 -0700

    repa-e

--- run_one.sh
+++ run_one.sh
@@ -1,9 +1,9 @@
 
 python main.py --data_folder pset/ \
-    --problems advantage-alignment \
+    --problems REPA-E \
     --gen \
-    --output_dir outputs/eval_harper_dev/advantage-alignment/ \
-    --llm_types GEMINI_2_0_FLASH \
+    --output_dir outputs/eval_harper_dev/REPA-E3/ \
+    --llm_types O3_MINI_HIGH \
     --n_completions 1 \
     --temperature 0.8 \
     --summarize_results \
