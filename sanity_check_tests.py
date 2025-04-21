e27464c0994323885fafcb76c35cf742c55a5d0d
patrickhua <patrickhua.ty@gmail.com>
2025-04-21 09:16:28 -0700

    yaml update

--- sanity_check_tests.py
+++ sanity_check_tests.py
@@ -1,9 +1,19 @@
 import yaml
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+import argparse
 
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# eomt 
+# advantage-alignment
+# REPA-E
 def main():
-    pset_dir = "pset"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pset_dir", type=str, default="pset")
+    parser.add_argument("--folder_name", nargs="+", type=str, default=None)
+    args = parser.parse_args()
+
+    pset_dir = args.pset_dir
     with open(f"{pset_dir}/papers.yaml", "r") as f:
         papers = yaml.safe_load(f)
 
@@ -12,8 +22,8 @@ def main():
 
     for paper in papers:
         folder_name = paper["id"]
-        # if folder_name != "GMFlow":
-        #     continue
+        if args.folder_name is not None and folder_name not in args.folder_name:
+            continue
         folder_path = f"{pset_dir}/{folder_name}"
         if not os.path.exists(folder_path):
             print(f"Folder {folder_path} does not exist")
